6d32cfa854afad5ad8d694cd2f6e47c85838d8b1
Kevin Caffrey <kevin@yext.com>
2011-02-01 15:22:20 -0500

    Add ability to pass JVM arguments to the convenient java run script via JVM_ARGS='-Dfoo=blah ...' build/Foo/Foo (transplanted from bb23b6ee12255193f80a94ab1717484ad6957f0f)
    --HG--
    extra : transplant_source : %BB%23%B6%EE%12%25Q%93%F8%0A%94%AB%17%17HJ%D6%95%7F%0F

--- icbm/java_run.sh
+++ icbm/java_run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#
+# Specify the JVM_ARGS environment variable to contain any other JVM args 
+# needed
 
 BASE=`dirname $0`
-exec java -cp ${BASE}/classes:${BASE}/jars/* %(main_class)s "$@"
+exec java -cp ${BASE}/classes:${BASE}/jars/* ${JVM_ARGS} %(main_class)s "$@"
