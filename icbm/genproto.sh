c4e67bffdbde0ee1edcaf85b6086b464956fdc99
Ilia Mirkin <ilia@yext.com>
2011-04-14 00:17:40 -0400

    Check in a protoc binary that will run on most people's setups. Update the genproto script to run it if it looks like it should execute. (transplanted from 1dd17fc94e00ba8209b08f5e0234e27659f36931)
    --HG--
    extra : transplant_source : %1D%D1%7F%C9N%00%BA%82%09%B0%8F%5E%024%E2vY%F3i1

--- icbm/genproto.sh
+++ icbm/genproto.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-PROTOC=${PROTOC:-protoc}
+if [ `uname -s` = "Linux" -a `uname -m` = "x86_64" ]; then
+  PROTOC=${PROTOC:-`dirname $0`/protoc}
+else
+  PROTOC=${PROTOC:-protoc}
+fi
 
 exec ${PROTOC} -I. --java_out=. "$1"
