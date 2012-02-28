612339e6a7683291d25d13efcacc6679b0f0c7f8
Austin Chu <eefi@yext.com>
2012-02-28 17:26:04 -0500

    Drop cast from warnings that trigger ICBM build failures
    ANTLR unfortunately generates lexers that do unnecessary casts to char.
    This was previously worked around with a sed during ICBM build, but that
    doesn't get applied to the files generated by the ANTLR plugin for
    Eclipse. This change supports Eclipse users better.
    (transplanted from eb463a36a88561aff91eb046ad1c2fc78b1cb1bc)
    
    --HG--
    extra : transplant_source : %EBF%3A6%A8%85a%AF%F9%1E%B0F%AD%1C/%C7%8B%1C%B1%BC

--- icbm/genantlr.sh
+++ icbm/genantlr.sh
@@ -5,9 +5,3 @@ OUTPUTDIR="$(dirname "$GRAMMAR")"
 
 java -classpath ../../Core/jars/antlr-2.7.6.jar \
      antlr.Tool -o "$OUTPUTDIR" "$GRAMMAR"
-
-# Suppress "redundant cast to char" warnings.
-LEXERS=$(grep 'class \(.*\) extends Lexer;' "$GRAMMAR" | \
-         sed -e 's/^.*class \(.*\) extends Lexer;.*$/\1.java/')
-cd "$OUTPUTDIR"
-sed -i.orig -e 's/(char)\(LA([0-9]\+)\)/\1/g' $LEXERS
